976c6d5e10092e03739b92ef7110f2df08e17609
AnanYoyo <596105081@qq.com>
2024-04-01 10:12:27 +0800

    - ware coupon

---
+++ mall-coupon/src/test/java/per/zqa/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package per.zqa.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
